718bfbd97bbc26afb3be0967516eddc0e45043be
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2024-08-23 19:10:09 +0300

    Sidebar menu doesn't work

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
+from pathlib import Path
 from setuptools import find_packages, setup
 
+PACKAGE_NAME = "netbox-plugin-buildingplan"
+
+def get_version():
+    version_file = Path(__file__).parent / PACKAGE_NAME.replace("-", "_") / "version.py"
+    with version_file.open() as f:
+        for line in f.readlines():
+            if "__version__" not in line:
+                continue
+            delimiter = "'" if "'" in line else '"'
+            return line.split(delimiter)[1]
+    raise RuntimeError("Could not find the version number")
+
 setup(
-    name='netbox_plugin_buildingplan',
-    version='1.0.0',
-    description='A NetBox plugin to add building plans to tenants.',
+    name=PACKAGE_NAME,
+    version=get_version(),
+    description="A NetBox plugin to add building plans to tenants.",
+    url="https://https://github.com/ChernNic/netbox_plugin_buildingplan",  
+    author="Your Name",
+    license="MIT",
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
